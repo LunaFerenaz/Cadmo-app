b8947126f5271d3c06f9f50b770667e9f4830d67
Cadmo <cadmo@Cadmos-MacBook-Air.local>
2026-05-31 20:09:44 -0300

    agregar base para github pages

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,21 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/Cadmo-app/',
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['icons/*.png'],
       manifest: {
-        name: 'Mi App',
-        short_name: 'MiApp',
+        name: 'Cadmo App',
+        short_name: 'Cadmo',
         description: 'Tracker de oficina y más',
         theme_color: '#111111',
         background_color: '#111111',
         display: 'standalone',
         orientation: 'portrait',
-        start_url: '/',
+        start_url: '/Cadmo-app/',
         icons: [
           { src: 'icons/icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: 'icons/icon-512.png', sizes: '512x512', type: 'image/png' }
